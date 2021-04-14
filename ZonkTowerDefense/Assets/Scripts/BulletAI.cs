d3f143eded236e3a3963480d49ebb02ec622f5ad
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-04-14 11:55:57 +0300

    Added explanatory comments & refactor code

--- ZonkTowerDefense/Assets/Scripts/BulletAI.cs
+++ ZonkTowerDefense/Assets/Scripts/BulletAI.cs
@@ -8,40 +8,44 @@ public class BulletAI : MonoBehaviour
     public float speed = 70f;
     public GameObject impactEffect;
     
+    //Seek is called from TurretAI.cs so we can set the closest enemy as the target.
     public void Seek (Transform temp_target){
         target = temp_target;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If we have no target, destroy the bullet.
         if (target == null){
             Destroy(gameObject);
             return;
         }
-
+        
+        //Calculate the distance to the target.
         Vector3 direction = target.position - transform.position;
+        //distanceThisFrame is a variable that stores the distance that the bullet is going to travel this frame.
         float distanceThisFrame = speed * Time.deltaTime;
 
+        //if the distance that we are going to travel this frame is greater than the distance to the enemy, that means that we are going to hit it.
         if (direction.magnitude <= distanceThisFrame){
             HitTarget();
             return;
         }
 
+        //If we didn't hit an enemy, we continue traveling as normal.
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
     }
 
     void HitTarget(){
+        //Add a particle effect to the scene.
         GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effect, 2f);
 
-        //temporary
+        //Destroy the enemy. Temporary since enemies have no health and bullet don't have a damage variable.
         Destroy(target.gameObject);
+
+        //Destroy the bullet.
         Destroy(gameObject);
     }
 }
--- ZonkTowerDefense/Assets/Scripts/TurretAI.cs
+++ ZonkTowerDefense/Assets/Scripts/TurretAI.cs
@@ -20,11 +20,16 @@ public class TurretAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //We use UpdateTarget(a custom function) instead of Update because we don't need to do calculations every frame
+        //as that would take a lot of computing power. Instead we invoke our custom funcion every 0.5s.
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
+    //a custom function that calculates the nearest enemy to the Turret.
     void UpdateTarget(){
+        //Get a list of all the enemies that are in the scene.
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        //Calculate the distance between the Turret and all the enemies in the above list and determine the one closest to the turret.
         float shortestDistance = Mathf.Infinity;
         GameObject nearestEnemy = null;
         foreach (GameObject enemy in enemies){
@@ -38,20 +43,23 @@ public class TurretAI : MonoBehaviour
         if (nearestEnemy != null && shortestDistance <= range){
             target = nearestEnemy.transform;
         } else{
+            //make sure that if we didn't find an enemy we set the target to null.
             target = null;
         }
     }
 
-    // Update is called once per frame
+    //We use the Update function in order to rotate and shoot with our Turret.
     void Update()
     {
         if (target == null) return;
 
+        //Bellow we calculate the rotation we need to make with Quaternions. The only part we rotate is partToRotate as is defined in Unity.
         Vector3 direction = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        //After the Turret is rotated, it shoots at the nearest enemy, the target.
         if (fireCountdown <= 0f){
             turretShoot();
             fireCountdown = 1f / fireRate;
@@ -61,14 +69,18 @@ public class TurretAI : MonoBehaviour
     }
 
     void turretShoot(){
+        //Add a bullet object to the scene.
         GameObject bulletObj = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //set a bullet object.
         BulletAI bullet = bulletObj.GetComponent<BulletAI>();
-
+        
+        //Seek is a funcion in BulletAI.cs with which we set the bullet target, aka. the closest enemy.
         if (bullet != null){
             bullet.Seek(target);
         }
     }
 
+    //a custom function that shows the Turret range in Unity.
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
