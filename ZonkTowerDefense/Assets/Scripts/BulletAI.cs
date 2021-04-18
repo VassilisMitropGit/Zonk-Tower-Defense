d063626658da8d27080c4dc325802db1f491bb78
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-04-18 14:17:21 +0300

    Added missile effects and functionality for the missile launcher

--- ZonkTowerDefense/Assets/Scripts/BulletAI.cs
+++ ZonkTowerDefense/Assets/Scripts/BulletAI.cs
@@ -6,6 +6,7 @@ public class BulletAI : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
     
     //Seek is called from TurretAI.cs so we can set the closest enemy as the target.
@@ -35,17 +36,44 @@ public class BulletAI : MonoBehaviour
 
         //If we didn't hit an enemy, we continue traveling as normal.
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        //Make so every bullet properly rotates and look to the enemy target.
+        transform.LookAt(target);
     }
 
     void HitTarget(){
         //Add a particle effect to the scene.
         GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effect, 2f);
+        Destroy(effect, 5f);
 
-        //Destroy the enemy. Temporary since enemies have no health and bullet don't have a damage variable.
-        Destroy(target.gameObject);
+        if (explosionRadius > 0f){
+            Explode();
+        }else{
+            //Destroy the enemy. Temporary since enemies have no health and bullet don't have a damage variable.
+            Damage(target);
+        }
 
         //Destroy the bullet.
         Destroy(gameObject);
+    }
+
+    void Damage(Transform enemy){
+        Destroy(enemy.gameObject);
+    }
+
+    void Explode(){
+        //Store all the objects that are in the radius in an array.
+        Collider [] objectsAffected = Physics.OverlapSphere(transform.position, explosionRadius);
+        //Among the above objects, find the enemies through the enemy tag.
+        foreach (Collider objectHit in objectsAffected)
+        {
+            if (objectHit.tag == "Enemy"){
+                Damage(objectHit.transform);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
